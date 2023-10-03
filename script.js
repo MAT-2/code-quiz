db291a61bcf482b6ed12e359c83afe9525df39c9
MAT-2 <mpcasey30@gmail.com>
2023-10-03 15:27:35 -0500

    I have a functioning timer working! I learned since the game.html is a separate file to the home page, the start button would not be able to work. So, I commented out the JS from the homepage since we are not using it, and made the function active as it is for any page was a script.js file. This will make it look like everytime you get into the page with the questions, the timer starts. I created two different global variables, and within the tickTock() we are calling, I set the number of seconds to use, made counter to be setInterval method calling the timer() function for every 1000 miliseconds. Once it reaches 0, we get a Stop Quiz text and clear interval. Also within the if statement, we added a for loop for the questionEl variable since they are looked as arrays when using querySelectorAll, and calling it by index to display as none and make the inputEL to display as block after the for loop is complete.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-var timer = document.getElementById("timer");
-var startButton = document.getElementById("startButton");
+var timerEl = document.querySelector(".timeLeft");
+//querySelector applies to only one. so do a querySelectorAll to basically create an array of elements JS is collecting the classes from.
+var questionEl = document.querySelectorAll(".questionSection");
+var inputEl = document.querySelector("#game-complete");
+// var startButtonEl = document.querySelector(".startButton");
+var counter;
+var time;
 
-startButton.addEventListener("click", tickTock);
+//Since we are going on a new page, the startButton does not exist on the new game.html page.
+// startButtonEl.addEventListener("click", tickTock);
+//in the setInterval parentheses, it is calling the timer function every 1000 miliseconds.
+
+//In For Loop, since querySelectorAll makes everything selected act as an array, we can use a For Loop to call each index of the question Sections to display as none.
 
 function tickTock() {
-  var timeTotal = 60;
-
-  var timeInterval = setInterval(function () {
-    timeTotal--;
-    timer.textContent;
-
-    if (timeTotal === 0) {
-      clearInterval(timeInterval);
+  time = 5;
+  counter = setInterval(timer, 1000);
+  function timer() {
+    time--;
+    timerEl.textContent = time;
+    if (time === 0) {
+      clearInterval(counter);
+      timerEl.textContent = "Stop Quiz!";
+      for (i = 0; i < questionEl.length; i++) {
+        questionEl[i].style.display = "none";
+      }
+      inputEl.style.display = "block";
     }
-  }, 1000);
+  }
 }
+tickTock();
